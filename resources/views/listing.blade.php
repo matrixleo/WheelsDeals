5f5fb3dca0634440cc9d3e8800e24c71117b081c
Leon <73724759+matrixleo@users.noreply.github.com>
2022-06-20 23:25:28 +0200

    Adding HTML template
    Template(Header, Footer), Logo, Images, etc.

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,6 +1,10 @@
+@extends('layout')
+
+@section('component')
 <h2>
     {{$listing['title']}}
 </h2>
 <p>
     {{$listing['description']}}
 </p>
+@endsection
---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,28 @@
+<section
+            class="relative h-72 bg-laravel flex flex-col justify-center align-center text-center space-y-4 mb-4"
+                style="
+                background-color: rgba(0, 0, 0, 0.8);
+                background-image: url('images/cover.jpg')"
+                id="hero"
+            >
+            <div
+                class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
+                id="carsphoto"
+            ></div>
+
+            <div class="z-10">
+                <h1 class="text-6xl font-bold uppercase text-white">
+                    WHEELS & DEALS
+                </h1>
+                <p class="text-2xl text-gray-200 font-bold my-4">
+                    Find or post newst cars
+                </p>
+                <div>
+                    <a
+                        href="register.html"
+                        class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+                        >Sign Up to Post a Car</a
+                    >
+                </div>
+            </div>
+        </section>
