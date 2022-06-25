37a7ce37e1af049d74e38f5bb704f003112027b2
Leon <73724759+matrixleo@users.noreply.github.com>
2022-06-25 16:05:20 +0200

    +Relationship User-Post(Listing)

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -23,4 +23,10 @@ class Listing extends Model
             ;
         }
     }
+
+    //Relationship with User
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Listing;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
-        Listing::factory(6)->create();
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
 
         // \App\Models\User::factory()->create([
